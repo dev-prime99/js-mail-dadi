5498c0315c64ad6bfefc2066695ad4180f04a6ff
dev-prime99 <prime99@icloud.com>
2019-10-21 21:43:29 +0200

    complited dadi ex

--- Dadi/assets/js/main.js
+++ Dadi/assets/js/main.js
@@ -1,15 +1,19 @@
 var player, computer;
 
-player = Math.floor((Math.random() * 6) + 1);
-console.log(player);
+function gioca(){
+  player = Math.floor((Math.random() * 6) + 1);
+  document.getElementById('cas1').innerHTML = player;
+  console.log(player);
 
-computer = Math.floor((Math.random() * 6) + 1);
-console.log(computer);
+  computer = Math.floor((Math.random() * 6) + 1);
+  document.getElementById('cas2').innerHTML = computer;
+  console.log(computer);
 
-if(player > computer) {
-  console.log("hai vinto");
-} else if(player < computer) {
-  console.log("hai perso");
-} else{
-  console.log("pareggio");
+  if(player > computer) {
+    document.getElementById('risultato').innerHTML = "hai vinto";
+  } else if(player < computer) {
+    document.getElementById('risultato').innerHTML = "hai perso";
+  } else{
+    document.getElementById('risultato').innerHTML = "pareggio";
+  }
 }
