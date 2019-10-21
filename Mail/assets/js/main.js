d5efc984aa542a0ac8e84dbb7d219dff21bfa209
dev-prime99 <prime99@icloud.com>
2019-10-21 19:41:37 +0200

    completed mail ex

---
+++ Mail/assets/js/main.js
@@ -0,0 +1,26 @@
+
+var utente;
+
+var iscritti = ["luca", "toni", "bepi", "nane", "ugo"];
+
+iscritti[0] = "luca@gmail.com";
+iscritti[1] = "toni@gmail.com";
+iscritti[2] = "bepi@gmail.com";
+iscritti[3] = "nane@gmail.com";
+iscritti[4] = "ugo@gmail.com";
+
+function controllo(){
+  utente = document.getElementById("put").value;
+
+  for (var i = 0; i < iscritti.length; i++){
+    if(utente == iscritti[i]){
+      //console.log("ok");
+      var ok = 1;
+    }
+    if(ok == 1) {
+      document.getElementById("risultato").innerHTML = "L'evento si terrà alle 22.00 della prossima settimana a casa mia";
+    } else{
+      document.getElementById("risultato").innerHTML = "Mi dispiace, non sei stato invitato";
+    }
+  }
+}
